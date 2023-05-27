8c7ee28c7a19972c63cb2e6121a896d26aec5770
Roman Tazhdynov <105038717+tajdinov@users.noreply.github.com>
2023-05-27 12:37:35 +0930

    1

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,12 +24,15 @@ const Home = () => {
     const fetchPosts = async () => {
       setloading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          "https://picturegen.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
         if (response.ok) {
           const result = await response.json();
           setallPosts(result.data.reverse());
